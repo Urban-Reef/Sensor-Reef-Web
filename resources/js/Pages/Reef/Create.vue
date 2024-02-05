4399f1cb508aab0fa461ddcd3b6b3233fbe43f0d
Daan <danielmeijs@gmail.com>
2024-02-05 17:28:01 +0100

    Added logic to dynamically add and remove points and sensors.

--- resources/js/Pages/Reef/Create.vue
+++ resources/js/Pages/Reef/Create.vue
@@ -6,11 +6,39 @@ const form = useForm({
     name: '',
     placedOn: '',
     long: '',
-    lat: ''
+    lat: '',
+    points: [{
+        sensors: [{
+            type: '',
+            unit: '',
+        }]
+    }]
 });
 
-const handleSubmit = () => {
-    form.post('/reefs');
+const addPoint = () => {
+    const newPoint = {
+        //sensor object for readability.
+        sensors: [{
+            type: '',
+            unit: '',
+        }]
+    };
+
+    form.points.push(newPoint);
+}
+const removePoint = (index) => {
+    form.points.splice(index, 1);
+}
+
+const addSensor = (pointIndex) => {
+    form.points[pointIndex].sensors.push({
+        type: '',
+        unit: '',
+    });
+}
+const removeSensor = (pointIndex, sensorIndex) => {
+    //Find the right point using pointIndex, then delete the sensor using Splice.
+    form.points[pointIndex].sensors.splice(sensorIndex, 1);
 }
 
 </script>
@@ -18,9 +46,9 @@ const handleSubmit = () => {
 <template>
     <h1>Set-up Reef</h1>
     <section>
-        <div>
-            <h2>General Information</h2>
-            <form @submit.prevent="form.post('/reefs')">
+        <form @submit.prevent>
+            <div class="subsection">
+                <h2>General Information</h2>
                 <label for="name">Name:</label>
                 <input
                     v-model="form.name"
@@ -47,9 +75,39 @@ const handleSubmit = () => {
                     type="number"
                 />
 <!--                TODO: Add points form-->
-                <button type="submit" :disabled="form.processing">Submit</button>
-            </form>
-        </div>
+            </div>
+            <div class="subsection" v-for="(point, pointIndex) in form.points" :key="pointIndex">
+                <h2>Point {{pointIndex + 1}}</h2>
+                <table v-show="point.sensors.length > 0">
+                    <tr>
+                        <th>Type:</th>
+                        <th>Unit:</th>
+                        <th></th>
+                    </tr>
+                    <tr v-for="(sensor, sensorIndex) in point.sensors" :key="`${pointIndex}.${sensorIndex}`">
+                        <td>
+                            <input
+                                v-model="sensor.type"
+                                type="text"
+                            />
+                        </td>
+                        <td>
+                            <input
+                                v-model="sensor.unit"
+                                type="text"
+                            />
+                        </td>
+                        <td>
+                            <button @click="removeSensor(pointIndex, sensorIndex)">Delete</button>
+                        </td>
+                    </tr>
+                </table>
+                <button @click="addSensor(pointIndex)">Add sensor</button>
+                <button @click="removePoint">Delete</button>
+            </div>
+            <button @click="addPoint">Add Point</button>
+
+        </form>
     </section>
 </template>
 
